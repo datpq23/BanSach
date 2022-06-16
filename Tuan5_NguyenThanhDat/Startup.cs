fca66aad7767bf5f7c754aaf6861e3acdac8470a
datpq23 <103091209+datpq23@users.noreply.github.com>
2022-06-16 16:08:57 +0700

    Add project files.

---
+++ Tuan5_NguyenThanhDat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tuan5_NguyenThanhDat.Startup))]
+namespace Tuan5_NguyenThanhDat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
