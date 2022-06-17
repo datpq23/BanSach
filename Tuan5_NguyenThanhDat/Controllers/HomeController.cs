4f63192aa785de49e2c0a26d6e64dae9b56c5d04
datpq23 <103091209+datpq23@users.noreply.github.com>
2022-06-17 15:07:04 +0700

    Cập nhật srouce

--- Tuan5_NguyenThanhDat/Controllers/HomeController.cs
+++ Tuan5_NguyenThanhDat/Controllers/HomeController.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tuan5_NguyenThanhDat.Models;
+using PagedList;
 
 namespace Tuan5_NguyenThanhDat.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //GET: Order
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index(int ? page)
         {
-            return View();
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Saches select s).OrderBy(n => n.masach);
+            int pageSize = 3;
+            int pageNum = page ?? 1;
+
+            return View(all_sach.ToPagedList(pageNum, pageSize));
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
